eda864364b21fae1620a687a8c89b73a408d14f8
Nawres Hajabouda <nawres.haj-abouda@etu.u-pec.fr>
2025-12-15 16:40:05 +0100

    commit erreur

--- ms-order/src/main/java/com/episen/order/infrastructure/exception/ProductNotFoundException.java
+++ ms-order/src/main/java/com/episen/order/infrastructure/exception/ProductNotFoundException.java
@@ -1,8 +1,7 @@
 package com.episen.order.infrastructure.exception;
 
 public class ProductNotFoundException extends RuntimeException {
-
     public ProductNotFoundException(Long productId) {
-        super("Produit non trouvé pour id=" + productId);
+        super("Produit introuvable pour id=" + productId);
     }
 }
